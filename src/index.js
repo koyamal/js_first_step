b440371ff9800a1cea1bf25c679616e104b05795
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-15 08:50:56 +0900

    Check arrow function Conventional: func1 Arrow: func2, func3

--- src/index.js
+++ src/index.js
@@ -35,11 +35,32 @@
 /**
  * テンプレート文字列
  */
-const name = "Tom";
-const age = 28;
-//「私の名前はTomです。年齢は28歳です。」
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
+// const name = "Tom";
+// const age = 28;
+// //「私の名前はTomです。年齢は28歳です。」
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
 
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// //従来の関数
+// // function func1(str){
+// //   return str;
+// // }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(20, 15));
