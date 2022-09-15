b9af2a14acbecc1df9975762c7cee796f6379acb
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-15 20:54:13 +0900

    Check map and filter usage

--- src/index.js
+++ src/index.js
@@ -122,3 +122,35 @@
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["tanaka", "yamada", "suzuki"];
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const newNameArr = nameArr.map((name) =>{
+//   if (name === "yamada"){
+//     return name
+//   }else{
+//     return `${name}さん`
+//   }
+// });
+// console.log(newNameArr);
